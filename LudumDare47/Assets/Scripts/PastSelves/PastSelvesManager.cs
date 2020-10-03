5047004d46025a0a64e3bee349f3df77a8f79491
Ian <russmoonbase@gmail.com>
2020-10-03 09:04:33 -0600

    Adding past selves

---
+++ LudumDare47/Assets/Scripts/PastSelves/PastSelvesManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PastSelf
+{
+   public Vector3 transform;
+   public Quaternion rotation;
+
+   public PastSelf(Vector3 carTransform, Quaternion carRotation)
+   {
+      this.transform = carTransform;
+      this.rotation = carRotation;
+   }
+}
+
+public class PastSelvesManager : MonoBehaviour
+{
+   public Transform SpawnPoint;
+   public GameObject pastSelfCar;
+
+   public bool isRecording;
+   public bool canSpawn;
+   public Transform thePlayer;
+   public List<PastSelf> pastSelves;
+   private PastSelf lastPastSelfRecording;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (isRecording)
+      {
+         if (thePlayer.position != lastPastSelfRecording.transform || thePlayer.rotation != lastPastSelfRecording.rotation)
+         {
+            var newPastSelf = new PastSelf(thePlayer.position, thePlayer.rotation);
+            pastSelves.Add(newPastSelf);
+
+            lastPastSelfRecording = newPastSelf;
+         }
+      }
+
+      if (canSpawn)
+      {
+         Instantiate(pastSelfCar, SpawnPoint.position, Quaternion.identity);
+         Instantiate(pastSelfCar, SpawnPoint.position, Quaternion.identity);
+         canSpawn = false;
+      }
+   }
+}
