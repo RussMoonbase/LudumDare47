b24c18aa5497de2f4ad1d41da434fcbb294e1d88
Ian <russmoonbase@gmail.com>
2020-10-02 20:25:34 -0600

    Added the car to the scene

--- LudumDare47/Assets/Scripts/CarController.cs
+++ LudumDare47/Assets/Scripts/CarController.cs
@@ -8,7 +8,11 @@ public class CarController : MonoBehaviour
    public Transform[] wheelMeshes;
 
    [SerializeField] private float torque = 200f;
-   [SerializeField] private float acceleration;
+   [SerializeField] private float steeringAmount = 30f;
+   private float acceleration;
+   private float steering;
+   private Vector3 wheelPostion;
+   private Quaternion wheelRotation;
 
    // Start is called before the first frame update
    void Start()
@@ -20,17 +24,30 @@ public class CarController : MonoBehaviour
    void Update()
    {
       acceleration = Input.GetAxis("Vertical");
-      Drive(acceleration);
+      steering = Input.GetAxis("Horizontal");
+      Drive(acceleration, steering);
    }
 
-   void Drive(float accel)
+   void Drive(float accel, float steer)
    {
       accel = Mathf.Clamp(accel, -1, 1);
+      steer = Mathf.Clamp(steer, -1, 1);
       //accel = accel * torque;
 
       for (int i = 0; i < wheelCols.Length; i++)
       {
          wheelCols[i].motorTorque = accel * torque;
+
+         if (i < 2)
+         {
+            wheelCols[i].steerAngle = steer * steeringAmount;
+         }
+
+
+         wheelCols[i].GetWorldPose(out wheelPostion, out wheelRotation);
+         wheelMeshes[i].transform.position = wheelPostion;
+         wheelMeshes[i].transform.localRotation = wheelRotation;
+
       }
    }
 }
