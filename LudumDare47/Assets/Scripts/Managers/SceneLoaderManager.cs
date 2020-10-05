c27d9f9c649d4c1dcba836463c2ac3e254b4fa8f
Ian <russmoonbase@gmail.com>
2020-10-05 00:53:50 -0600

    Added win, lose, and main menu scenes

---
+++ LudumDare47/Assets/Scripts/Managers/SceneLoaderManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderManager : MonoBehaviour
+{
+   public static SceneLoaderManager instance;
+   private int sceneNumber;
+
+   private void Awake()
+   {
+      instance = this;
+   }
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   public void LoadNextScene()
+   {
+      sceneNumber = SceneManager.GetActiveScene().buildIndex;
+      sceneNumber++;
+      SceneManager.LoadScene(sceneNumber);
+   }
+
+   public void LoadStartScene()
+   {
+      SceneManager.LoadScene(0);
+   }
+}
